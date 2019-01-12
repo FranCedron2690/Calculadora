b2f7dab739c916adb3c33dac168f67f8327492b2
Fran de la Jungla <fran.j2690@hotmail.com>
2019-01-12 21:22:48 +0100

    Calculadora UWP

--- Calculadora/MainPage.xaml.cs
+++ Calculadora/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Sensors;
+using Windows.Graphics.Display;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
@@ -25,6 +28,42 @@ namespace Calculadora
         public MainPage()
         {
             this.InitializeComponent();
+            this.SizeChanged += Page_SizeChanged;
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Debug.WriteLine(e.NewSize.Width + " ," + e.NewSize.Height);
+
+            //Landscape
+            if (e.NewSize.Width > e.NewSize.Height)
+            {
+                Debug.WriteLine("Landscape");
+            }
+            else //Portrait
+            {
+                Debug.WriteLine("Portrait");
+            }
+
+            //Otra forma de obtener la orientación de la pantalla
+            /*DisplayInformation displayInfo = DisplayInformation.GetForCurrentView();
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Portrait;
+
+            switch (displayInfo.CurrentOrientation)
+            {
+                case DisplayOrientations.Landscape:
+                    Debug.WriteLine("Landscape");
+                    break;
+                case DisplayOrientations.Portrait:
+                    Debug.WriteLine("Portrait");
+                    break;
+                case DisplayOrientations.LandscapeFlipped:
+                    Debug.WriteLine("LandscapeFlipped");
+                    break;
+                case DisplayOrientations.PortraitFlipped:
+                    Debug.WriteLine("PortraitFlipped");
+                    break;
+            }*/
         }
     }
 }
